076bafb5a37537ceabb9d6d6842ef73a0cf1052e
lelethu <lelethukosana@aol.com>
2023-06-26 01:01:06 +0200

    commit

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws"{
     region =  "us-east-1"
-    access_key = var.access_key
-    secret_key = var.secret_key
+    access_key = "4SrbRdsYrUvwTLOFY1O7LI2eVZoxGY2snuQfyquk"
+    secret_key = "4SrbRdsYrUvwTLOFY1O7LI2eVZoxGY2snuQfyquk"
 }
--- variable.tf
+++ variable.tf
@@ -1,6 +1,1 @@
-variable "access_key" {
-  keydefault = "AKIA5IKNJJO2UOA4OUG7"
-}
-variable "secret_key" {
-  keydefault  = "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"
-}
+//
